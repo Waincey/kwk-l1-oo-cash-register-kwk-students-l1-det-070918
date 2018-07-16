1b5da7c425f9f5e2423f8c03dfa8109eeecc4770
Waincey <waincey.chan@gmail.com>
2018-07-16 15:37:59 +0000

    automatically backed up by learn

--- amazon_transaction.rb
+++ amazon_transaction.rb
@@ -1,1 +1,34 @@
-# Code your cash register here!
+class AmazonTransaction
+  amazon_transaction = AmazonTransaction.new 
+  
+def initalize amazon_transaction
+  @total == 0 
+  @items == []
+  @discount == 20
+end 
+
+amazon_transaction.total = 100 
+eggs = AmazonTransaction.new
+eggs = 0.98
+
+
+end 
+
+def shipping 
+  puts "Do you have Amazon Rrime? (yes or no)"
+  gets.chomp
+  if answer = yes 
+    shipping = 0 
+  else 
+      shipping = __
+end
+
+def discounts 
+  puts "Are you an Amazon employee? (yes or no)"
+  gets.chomp
+  if answer = yes 
+    discount = 20 
+  else 
+    discouunt = 0 
+    
+    
